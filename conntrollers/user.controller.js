e738081422b0849e1bcb475959490636a5ab724f
abdukakhkhorov <abdukhkhorov@mail.ru>
2021-08-25 10:37:15 +0500

    add try catch

--- conntrollers/user.controller.js
+++ conntrollers/user.controller.js
@@ -3,38 +3,59 @@ const db = require("../db");
 class UserController {
   async createUser(req, res) {
     const { name, surname } = req.body;
-    const newPerson = await db.query(
-      `INSERT INTO person (name, surname) values($1, $2) Returning *`,
-      [name, surname]
-    );
+    try {
+      const newPerson = await db.query(
+        `INSERT INTO person (name, surname) values($1, $2) Returning *`,
+        [name, surname]
+      );
 
-    res.json(newPerson.rows[0]);
-    console.log("Created!");
+      res.json(newPerson.rows[0]);
+
+      console.log("Created!");
+    } catch (err) {
+      console.log(err);
+    }
   }
   async getUser(req, res) {
-    const users = await db.query(`SELECT * FROM person`);
-    res.json(users.rows);
+    try {
+      const users = await db.query(`SELECT * FROM person`);
+      res.json(users.rows);
+    } catch (err) {
+      console.log(err);
+    }
   }
   async getOneUser(req, res) {
-    const id = req.params.id;
-    const user = await db.query(`SELECT * FROM person where id = $1`, [id]);
-    res.json(user.rows[0]);
+    try {
+      const id = req.params.id;
+      const user = await db.query(`SELECT * FROM person where id = $1`, [id]);
+      res.json(user.rows[0]);
+    } catch (err) {
+      console.log(err);
+    }
   }
   async updateUser(req, res) {
-    const { id, name, surname } = req.body;
-    const user = await db.query(
-      `UPDATE person set name = $1, surname = $2 where id = $3 RETURNING *`,
-      [name, surname, id]
-    );
-    ``;
-    res.json(user.rows[0]);
-    console.log("Updated!");
+    try {
+      const { id, name, surname } = req.body;
+      const user = await db.query(
+        `UPDATE person set name = $1, surname = $2 where id = $3 RETURNING *`,
+        [name, surname, id]
+      );
+      ``;
+      res.json(user.rows[0]);
+      console.log("Updated!");
+    } catch (err) {
+      console.log(err);
+    }
   }
   async deleteUser(req, res) {
-    const id = req.params.id;
-    const user = await db.query(`DELETE FROM person where id = $1`, [id]);
-    res.json(user.rows[0]);
-    console.log("Deleted!");
+    try {
+      const id = req.params.id;
+      const user = await db.query(`DELETE FROM person where id = $1`, [id]);
+      res.json(user.rows[0]);
+      console.log("Deleted!");
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
